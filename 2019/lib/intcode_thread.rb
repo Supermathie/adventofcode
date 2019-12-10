0df93e68f449c061e18fbfb7727acdb18107fdf8
Michael Brown <supermathie@gmail.com>
2019-12-09 21:49:39 -0500

    intcode: add relative address support and 9a answer

--- 2019/lib/intcode_thread.rb
+++ 2019/lib/intcode_thread.rb
@@ -1,9 +1,26 @@
 #!/usr/bin/env ruby
 
 class IntcodeThread < Array
+  def resolv_addr(addr, mode)
+    case mode
+    when 0
+      self[addr]
+    when 1
+      addr
+    when 2
+      @reladd + self[addr]
+    end
+  end
+
+  def [](addr)
+    # we want unwritten memory to have a value of 0
+    super || 0
+  end
+
   def execute(input: nil, output: nil, trace: false)
     @input  = input  || Queue.new
     @output = output || Queue.new
+    @reladd = 0
 
     @thread = Thread.new {
       @pc = 0
@@ -13,34 +30,37 @@ class IntcodeThread < Array
         puts "#{self[@pc..@pc+3]} pc=#{@pc} opcode=#{opcode} modes=#{modes}" if trace
         if opcode == 1
           # add s0, s1, d
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
-          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} [ADD]" if trace
-          self[self[@pc+3]] = s0 + s1
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
+          d = resolv_addr(@pc+3, modes[2].to_i)
+          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{d} [ADD]" if trace
+          self[d] = s0 + s1
           @pc += 4
         elsif opcode == 2
           # mul s0, s1, d
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
-          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} [MUL]" if trace
-          self[self[@pc+3]] = s0 * s1
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
+          d = resolv_addr(@pc+3, modes[2].to_i)
+          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{d} [MUL]" if trace
+          self[d] = s0 * s1
           @pc += 4
         elsif opcode == 3
           # store [input], d
+          d = resolv_addr(@pc+1, modes[0].to_i)
           val = @input.pop
-          puts "#{self[@pc..@pc+2]} pc=#{@pc} loc=#{self[@pc+1]} old=#{self[self[@pc+1]]} new=#{val} [STO]" if trace
-          self[self[@pc+1]] = val
+          puts "#{self[@pc..@pc+2]} pc=#{@pc} d=#{d} old=#{self[d]} new=#{val} [STO]" if trace
+          self[d] = val
           @pc += 2
         elsif opcode == 4
           # output s0
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
           puts "#{self[@pc..@pc+2]} pc=#{@pc} s0=#{s0} [OUT]" if trace
           @output << s0
           @pc += 2
         elsif opcode == 5
           # jnz s0, s1
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
           puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} [JNZ]" if trace
           if s0 != 0
             @pc = s1
@@ -49,8 +69,8 @@ class IntcodeThread < Array
           end
         elsif opcode == 6
           # jz s0, s1
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
           puts "#{self[@pc..@pc+2]} pc=#{@pc} s0=#{s0} s1=#{s1} [JEZ]" if trace
           if s0 == 0
             @pc = s1
@@ -59,20 +79,28 @@ class IntcodeThread < Array
           end
         elsif opcode == 7
           # tlt s0, s1, d
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
+          d = resolv_addr(@pc+3, modes[2].to_i)
           val = s0 < s1 ? 1 : 0
-          self[self[@pc+3]] = val
-          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{val} [TLT]" if trace
+          self[d] = val
+          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{d} [TLT]" if trace
           @pc += 4
         elsif opcode == 8
           # teq s0, s1, d
-          s0 = self[@pc+1]; s0 = self[s0] if modes[0].to_i == 0
-          s1 = self[@pc+2]; s1 = self[s1] if modes[1].to_i == 0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          s1 = self[resolv_addr(@pc+2, modes[1].to_i)]
+          d = resolv_addr(@pc+3, modes[2].to_i)
           val = s0 == s1 ? 1 : 0
-          self[self[@pc+3]] = val
-          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{val} [TEQ]" if trace
+          self[d] = val
+          puts "#{self[@pc..@pc+3]} pc=#{@pc} s0=#{s0} s1=#{s1} d=#{d} [TEQ]" if trace
           @pc += 4
+        elsif opcode == 9
+          # sra s0
+          s0 = self[resolv_addr(@pc+1, modes[0].to_i)]
+          puts "#{self[@pc..@pc+1]} pc=#{@pc} s0=#{s0} [SRA]" if trace
+          @reladd += s0
+          @pc += 2
         else
           raise StandardError, "Unknown opcode #{self[@pc]} at pc: #{@pc}"
         end
--- 2019/spec/intcode_thread_spec.rb
+++ 2019/spec/intcode_thread_spec.rb
@@ -1,4 +1,3 @@
-require 'socket'
 require_relative '../lib/intcode_thread'
 
 describe IntcodeThread do
@@ -136,4 +135,33 @@ describe IntcodeThread do
     expect(comp2.output.pop).to eq(0)
   end
 
+  it "can use relative addresses" do
+    prog = [109,1,204,-1,99]
+    comp = IntcodeThread.new(prog).execute
+    comp.join
+    expect(comp.output.pop).to eq(109)
+  end
+
+  it "can use relative addresses (ex. 9a1)" do
+    prog = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    comp = IntcodeThread.new(prog).execute
+    comp.join
+    q = comp.output
+    expect(q.length.times.map { q.pop }).to eq(prog)
+  end
+
+  it "can use relative addresses (ex. 9a2)" do
+    prog = [1102,34915192,34915192,7,4,7,99,0]
+    comp = IntcodeThread.new(prog).execute
+    comp.join
+    expect(comp.output.pop).to eq(34915192*34915192)
+  end
+
+  it "can use relative addresses (ex. 9a3)" do
+    prog = [104,1125899906842624,99]
+    comp = IntcodeThread.new(prog).execute
+    comp.join
+    q = comp.output
+    expect(comp.output.pop).to eq(1125899906842624)
+  end
 end
