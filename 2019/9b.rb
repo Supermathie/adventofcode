c9c30d689d91e4f6ec3f3b97d8a2d31d7f8e44c4
Michael Brown <supermathie@gmail.com>
2019-12-09 21:51:10 -0500

    Add solution for 9b

---
+++ 2019/9b.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/intcode_thread'
+
+memory = File.open('9.input').readline.split(',').map(&:to_i)
+comp = IntcodeThread.new(memory).execute(input: [2])
+comp.join
+while comp.output.length > 0
+  puts comp.output.pop
+end
