a9d5dc339126a7f39c9426d0d8a931ce6af9106a
Michael Brown <supermathie@gmail.com>
2020-12-11 16:16:02 -0500

    Add placeholders for the rest of the days

---
+++ 2020/17b.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+
+	"supermathie.net/libadvent"
+)
+
+func day17b(inputFile string) (int, error) {
+	c, err := libadvent.ReadFileLines(inputFile)
+	if err != nil {
+		return -1, err
+	}
+
+	if c != nil {
+
+	}
+	return 0, errors.New("not implemented")
+}
