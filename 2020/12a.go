9d6c3ec0416019b6377a4b9db4a3da09c7fc0d13
Michael Brown <supermathie@gmail.com>
2020-12-12 18:14:44 -0500

    Add 2020 day 12 go solutions

--- 2020/12a.go
+++ 2020/12a.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"log"
+	"strconv"
 
 	"supermathie.net/libadvent"
 )
@@ -12,8 +15,43 @@ func day12a(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
-
+	x := 0
+	y := 0
+	heading := 90
+	for dir := range c {
+		action := dir[0]
+		val, _ := strconv.Atoi(dir[1:])
+		switch action {
+		case 'N':
+			y += val
+		case 'S':
+			y -= val
+		case 'E':
+			x += val
+		case 'W':
+			x -= val
+		case 'L':
+			heading = (heading - val + 360) % 360
+		case 'R':
+			heading = (heading + val + 360) % 360
+		case 'F':
+			switch heading {
+			case 0:
+				y += val
+			case 180:
+				y -= val
+			case 90:
+				x += val
+			case 270:
+				x -= val
+			default:
+				log.Fatalf("bad heading %d", heading)
+			}
+		default:
+			log.Fatalf("bad action %v", action)
+		}
 	}
+	fmt.Printf("%d, %d\n", x, y)
+
 	return 0, errors.New("not implemented")
 }
--- 2020/12b.go
+++ 2020/12b.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
 
 	"supermathie.net/libadvent"
 )
@@ -12,8 +15,48 @@ func day12b(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
-
+	x := 0
+	y := 0
+	dx := 10
+	dy := 1
+	for dir := range c {
+		action := dir[0]
+		val, _ := strconv.Atoi(dir[1:])
+		switch action {
+		case 'N':
+			dy += val
+		case 'S':
+			dy -= val
+		case 'E':
+			dx += val
+		case 'W':
+			dx -= val
+		case 'L':
+			switch val {
+			case 90:
+				dx, dy = -dy, dx
+			case 180:
+				dx, dy = -dx, -dy
+			case 270:
+				dx, dy = dy, -dx
+			}
+		case 'R':
+			switch val {
+			case 270:
+				dx, dy = -dy, dx
+			case 180:
+				dx, dy = -dx, -dy
+			case 90:
+				dx, dy = dy, -dx
+			}
+		case 'F':
+			x += dx * val
+			y += dy * val
+		default:
+			log.Fatalf("bad action %v", action)
+		}
 	}
-	return 0, errors.New("not implemented")
+	fmt.Printf("%d, %d\n", x, y)
+
+	return int(math.Abs(float64(x)) + math.Abs(float64(y))), nil
 }
