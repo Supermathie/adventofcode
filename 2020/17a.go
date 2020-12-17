344292632c43dc2f34e5bedd38a26ee792ad7282
Michael Brown <supermathie@gmail.com>
2020-12-17 01:22:19 -0500

    Add 2020 day 17 go solutions

--- 2020/17a.go
+++ 2020/17a.go
@@ -1,10 +1,79 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"log"
 
 	"supermathie.net/libadvent"
 )
+
+type point3 struct {
+	x, y, z int
+}
+
+func neighbours3(point point3) (n [27]point3) {
+	// we are *deliberately* counting ourselves
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				n[(x+1)*9+(y+1)*3+(z+1)] = point3{point.x + x, point.y + y, point.z + z}
+			}
+		}
+	}
+	return
+}
+
+func day17simulate3(state map[point3]bool) (newState map[point3]bool) {
+	neighbourCount := make(map[point3]int)
+	newState = make(map[point3]bool)
+	for point := range state {
+		for _, neighbour := range neighbours3(point) {
+			if _, present := neighbourCount[neighbour]; present {
+				neighbourCount[neighbour]++
+			} else {
+				neighbourCount[neighbour] = 1
+			}
+		}
+	}
+	for point, count := range neighbourCount {
+		if count == 3 || (state[point] == true && count == 4) {
+			newState[point] = true
+		}
+	}
+	return
+}
+
+func printState3(state map[point3]bool) {
+	minX, maxX, minY, maxY, minZ, maxZ := 0, 0, 0, 0, 0, 0
+	for point := range state {
+		minX, maxX = point.x, point.x
+		minY, maxY = point.y, point.y
+		minZ, maxZ = point.z, point.z
+		break
+	}
+	for point := range state {
+		minX = libadvent.Min([]int{point.x, minX})
+		minY = libadvent.Min([]int{point.y, minY})
+		minZ = libadvent.Min([]int{point.z, minZ})
+		maxX = libadvent.Max([]int{point.x, maxX})
+		maxY = libadvent.Max([]int{point.y, maxY})
+		maxZ = libadvent.Max([]int{point.z, maxZ})
+	}
+	for z := minZ; z <= maxZ; z++ {
+		fmt.Printf("z=%v, maxY=%v, minX=%v\n", z, maxY, minX)
+		for y := maxY; y >= minY; y-- {
+			for x := minX; x <= maxX; x++ {
+				if _, present := state[point3{x, y, z}]; present {
+					fmt.Print("#")
+				} else {
+					fmt.Print(".")
+				}
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+}
 
 func day17a(inputFile string) (int, error) {
 	c, err := libadvent.ReadFileLines(inputFile)
@@ -12,8 +81,30 @@ func day17a(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
+	state := map[point3]bool{}
 
+	{
+		x, y, z := 0, 0, 0
+		for line := range c {
+			for x = 0; x < len(line); x++ {
+				switch line[x] {
+				case '.':
+					// off, do nothing
+				case '#':
+					state[point3{x, y, z}] = true
+				default:
+					log.Fatalf("invalid char")
+				}
+			}
+			y--
+		}
 	}
-	return 0, errors.New("not implemented")
+
+	for t := 0; t < 6; t++ {
+		fmt.Printf("t=%v, count=%v\n", t, len(state))
+		// printState3(state)
+		state = day17simulate3(state)
+	}
+
+	return len(state), nil
 }
--- 2020/17b.go
+++ 2020/17b.go
@@ -1,10 +1,53 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"log"
 
 	"supermathie.net/libadvent"
 )
+
+type point4 struct {
+	w, x, y, z int
+}
+
+func neighbours4(point point4) (n [81]point4) {
+	// we are *deliberately* counting ourselves
+	for w := -1; w <= 1; w++ {
+		for x := -1; x <= 1; x++ {
+			for y := -1; y <= 1; y++ {
+				for z := -1; z <= 1; z++ {
+					n[(w+1)*27+(x+1)*9+(y+1)*3+(z+1)] = point4{point.w + w, point.x + x, point.y + y, point.z + z}
+				}
+			}
+		}
+	}
+	return
+}
+
+func day17simulate4(state map[point4]bool) (newState map[point4]bool) {
+	neighbourCount := make(map[point4]int)
+	newState = make(map[point4]bool)
+	for point := range state {
+		for _, neighbour := range neighbours4(point) {
+			if _, present := neighbourCount[neighbour]; present {
+				neighbourCount[neighbour]++
+			} else {
+				neighbourCount[neighbour] = 1
+			}
+		}
+	}
+	for point, count := range neighbourCount {
+		if count == 3 || (state[point] == true && count == 4) {
+			newState[point] = true
+		}
+	}
+	return
+}
+
+func printState4(state map[point4]bool) {
+	// this function left as an exercise for the reader
+}
 
 func day17b(inputFile string) (int, error) {
 	c, err := libadvent.ReadFileLines(inputFile)
@@ -12,8 +55,30 @@ func day17b(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
+	state := map[point4]bool{}
 
+	{
+		w, x, y, z := 0, 0, 0, 0
+		for line := range c {
+			for x = 0; x < len(line); x++ {
+				switch line[x] {
+				case '.':
+					// off, do nothing
+				case '#':
+					state[point4{w, x, y, z}] = true
+				default:
+					log.Fatalf("invalid char")
+				}
+			}
+			y--
+		}
 	}
-	return 0, errors.New("not implemented")
+
+	for t := 0; t < 6; t++ {
+		fmt.Printf("t=%v, count=%v\n", t, len(state))
+		printState4(state)
+		state = day17simulate4(state)
+	}
+
+	return len(state), nil
 }
