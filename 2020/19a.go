dd197e294272fdc63717f2bf7e5d6c0768f1293f
Michael Brown <supermathie@gmail.com>
2020-12-19 03:41:22 -0500

    Add 2020 day 19 go solutions

--- 2020/19a.go
+++ 2020/19a.go
@@ -1,10 +1,78 @@
 package main
 
 import (
-	"errors"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"supermathie.net/libadvent"
 )
+
+type rule19 struct {
+	num      int
+	terminal rune
+	options  [][]int
+}
+
+func expand19(grammar map[int]rule19, ruleNum int) (c chan string) {
+	c = make(chan string, 8)
+	go func() {
+		defer close(c)
+		rule := grammar[ruleNum]
+		if rule.terminal != 0 {
+			c <- string(rule.terminal)
+		} else {
+			for _, option := range rule.options {
+				for s1 := range expand19(grammar, option[0]) {
+					if len(option) > 1 {
+						for s2 := range expand19(grammar, option[1]) {
+							c <- s1 + s2
+						}
+					} else {
+						c <- s1
+					}
+				}
+			}
+		}
+	}()
+	return
+}
+
+func readGrammar(c chan string) map[int]rule19 {
+	ruleMatcher := regexp.MustCompile(`^(\d+): (?:"(a)"|"(b)"|(\d.*))$`)
+
+	rules := make(map[int]rule19)
+
+	for line := range c {
+		if line == "" {
+			break
+		}
+		match := ruleMatcher.FindStringSubmatch(line)
+		rule := rule19{}
+		if match == nil {
+			log.Fatalf("matcher did not match line:\n%v", line)
+		}
+		rule.num, _ = strconv.Atoi(match[1])
+		if match[2] == "a" {
+			rule.terminal = 'a'
+		} else if match[3] == "b" {
+			rule.terminal = 'b'
+		} else {
+			options := strings.Split(match[4], " | ")
+			rule.options = make([][]int, 0)
+			for i := 0; i < len(options); i++ {
+				rule.options = append(rule.options, make([]int, 0))
+				for _, s := range strings.Split(options[i], " ") {
+					ruleNum, _ := strconv.Atoi(s)
+					rule.options[i] = append(rule.options[i], ruleNum)
+				}
+			}
+		}
+		rules[rule.num] = rule
+	}
+	return rules
+}
 
 func day19a(inputFile string) (int, error) {
 	c, err := libadvent.ReadFileLines(inputFile)
@@ -12,8 +80,18 @@ func day19a(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
+	grammar := readGrammar(c)
 
+	possibleMessages := make(map[string]bool, 0)
+	for possibleMessage := range expand19(grammar, 0) {
+		possibleMessages[possibleMessage] = true
 	}
-	return 0, errors.New("not implemented")
+
+	validMessages := 0
+	for line := range c {
+		if possibleMessages[line] {
+			validMessages++
+		}
+	}
+	return validMessages, nil
 }
--- 2020/19b.go
+++ 2020/19b.go
@@ -1,10 +1,83 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"log"
 
 	"supermathie.net/libadvent"
 )
+
+type result struct {
+	len   int
+	start int
+	rule  int
+}
+
+func search19(grammar map[int]rule19, s string) bool {
+	possible := make(map[result]bool)
+	for i := 0; i < len(s); i++ {
+		for _, rule := range grammar {
+			if rule.terminal == rune(s[i]) {
+				possible[result{1, i, rule.num}] = true
+			}
+		}
+	}
+
+	for i := 0; i < len(s); i++ { // start of substring
+		changed := false
+		for _, rule := range grammar {
+			for _, option := range rule.options {
+				if len(option) == 1 {
+					if possible[result{1, i, option[0]}] {
+						if !possible[result{1, i, rule.num}] {
+							changed = true
+						}
+						possible[result{1, i, rule.num}] = true
+					}
+				}
+			}
+		}
+		if changed {
+			i-- // run it again
+		}
+	}
+
+	for l := 2; l <= len(s); l++ { // length of substring
+		changed := false
+		for i := 0; i <= len(s)-l+1; i++ { // start of substring
+			for p := 1; p <= l-1; p++ { // partition point of substring
+				for _, rule := range grammar {
+					if possible[result{l, i, rule.num}] {
+						continue // we already know this rule works
+					}
+					for _, option := range rule.options {
+						if len(option) == 1 {
+							if possible[result{l, i, option[0]}] {
+								possible[result{l, i, rule.num}] = true
+								changed = true
+							}
+						} else {
+							if len(option) != 2 {
+								log.Fatalf("rule:%d option:%v error 2", rule.num, option)
+							}
+							if possible[result{p, i, option[0]}] && possible[result{l - p, i + p, option[1]}] {
+								possible[result{l, i, rule.num}] = true
+								changed = true
+							}
+						}
+					}
+				}
+			}
+		}
+		if changed {
+			l-- // run it again
+		}
+	}
+
+	return possible[result{len(s), 0, 0}]
+}
+
+// 285 too low
 
 func day19b(inputFile string) (int, error) {
 	c, err := libadvent.ReadFileLines(inputFile)
@@ -12,8 +85,16 @@ func day19b(inputFile string) (int, error) {
 		return -1, err
 	}
 
-	if c != nil {
+	grammar := readGrammar(c)
 
+	validMessages := 0
+	for line := range c {
+		if search19(grammar, line) {
+			fmt.Printf("+:%s\n", line)
+			validMessages++
+		} else {
+			fmt.Printf("-:%s\n", line)
+		}
 	}
-	return 0, errors.New("not implemented")
+	return validMessages, nil
 }
