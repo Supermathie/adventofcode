af8d6df4962366b56cbb6af97ca5c4a2beef0aef
Michael Brown <supermathie@gmail.com>
2022-12-04 13:13:02 -0500

    Add 2022 day 4 solution

--- 2022/src/days/day04.rs
+++ 2022/src/days/day04.rs
@@ -1,12 +1,34 @@
+use std::io::BufRead;
+
+use crate::util;
 use crate::{Solution, SolutionPair};
-//use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = util::open_input("input/4.txt").lines();
+
+    let mut sol1: u64 = 0;
+    let mut sol2: u64 = 0;
+
+    for line in input {
+        let line = line.unwrap();
+        let pair: [[u64; 2]; 2] = line.split(',').map(
+            |p| p.split('-').map(|i|
+                i.parse::<u64>().unwrap()
+            ).collect::<Vec<u64>>().try_into().unwrap()
+        ).collect::<Vec<[u64; 2]>>().try_into().unwrap();
+
+        if (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) ||
+           (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1]) {
+            sol1 += 1;
+        }
+
+        if  (pair[1][0] <= pair[0][0] && pair[0][0] <= pair[1][1]) ||
+            (pair[0][0] <= pair[1][0] && pair[1][0] <= pair[0][1]) {
+            sol2 += 1;
+        }
+    }
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
