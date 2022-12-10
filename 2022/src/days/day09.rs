9594bb97d6f15ec82cea8e152ad032e3d847eca7
Michael Brown <supermathie@gmail.com>
2022-12-09 21:00:49 -0500

    2022 day 9: refactor solution
    The trap is trying to model a "rope" rather than a "rope segment". The logic
    and problem are much more clear when considering the modelling of segments.
    Nine knots -> ten segments.

--- 2022/src/days/day09.rs
+++ 2022/src/days/day09.rs
@@ -1,5 +1,3 @@
-use colored::Colorize;
-
 use std::collections::HashSet;
 use std::collections::HashMap;
 use std::io::BufRead;
@@ -17,23 +15,21 @@ struct Point {
     y: CoordSize,
 }
 
-struct Rope {
+struct RopeSegment {
     desc: char,
-    head: Point,
-    tail: Point,
-    next: Option<Box<Rope>>,
-    // on_tail_move: fn(Point),
+    loc: Point,
+    next: Option<Box<RopeSegment>>,
 }
 
-fn print_state(rope: &Rope) {
+#[allow(dead_code)]
+fn print_state(rope_segment: &RopeSegment) {
     let mut grid: HashMap<Point, char> = HashMap::new();
 
     grid.insert(Point { x: 0, y: 0 }, 's');
-    for d in 0..9 {
-        let seg = rope.get(d);
-        grid.insert(seg.head.clone(), seg.desc);
+    for d in 0..ROPE_LEN {
+        let seg = rope_segment.get(d);
+        grid.insert(seg.loc.clone(), seg.desc);
     }
-    grid.insert(rope.get(9).tail.clone(), '9');
 
     let x_min = grid.keys().map(|p| p.x).min().unwrap();
     let x_max = grid.keys().map(|p| p.x).max().unwrap();
@@ -54,6 +50,7 @@ fn print_state(rope: &Rope) {
     print!("\n");
 }
 
+#[allow(dead_code)]
 fn print_grid(grid: &HashSet<Point>) {
     let x_min = grid.iter().map(|p| p.x).min().unwrap();
     let x_max = grid.iter().map(|p| p.x).max().unwrap();
@@ -74,125 +71,119 @@ fn print_grid(grid: &HashSet<Point>) {
     print!("\n");
 }
 
-impl Rope {
-    fn move_tail(&mut self) {
-        let dx = self.head.x - self.tail.x;
-        let dy = self.head.y - self.tail.y;
-        if dx.abs() > 1 || dy.abs() > 1 {
-            let dx = dx.clamp(-1, 1);
-            let dy = dy.clamp(-1, 1);
-            self.tail.x += dx;
-            self.tail.y += dy;
-            // print!("move_tail: seg {} moving {}, {}\n", self.desc, dx, dy);
-            match self.next.as_deref_mut() {
-                None => (),
-                Some(n) => {
-                    n.move_head(dx, dy);
-                }
+impl RopeSegment {
+    fn move_next(&mut self) {
+        if let Some(next) = self.next.as_deref_mut() {
+            let dx = self.loc.x - next.loc.x;
+            let dy = self.loc.y - next.loc.y;
+
+            if dx.abs() > 1 || dy.abs() > 1 {
+                let dx = dx.clamp(-1, 1);
+                let dy = dy.clamp(-1, 1);
+                next.do_move(dx, dy);           
             }
-            // (self.on_tail_move)(self.tail);
         }
     }
-    fn move_head(&mut self, dx: CoordSize, dy: CoordSize) {
-        // print!("move_head: seg {} moving {}, {}\n", self.desc, dx, dy);
-        self.head.x += dx;
-        self.head.y += dy;
-        self.move_tail();
+    fn do_move(&mut self, dx: CoordSize, dy: CoordSize) {
+        // print!("do_move: seg {} moving {}, {}\n", self.desc, dx, dy);
+        self.loc.x += dx;
+        self.loc.y += dy;
+        self.move_next();
     }
     pub fn up(&mut self) {
         // print!("move_up\n");
-        self.move_head(0, -1);
+        self.do_move(0, -1);
     }
     pub fn down(&mut self) {
         // print!("move_down\n");
-        self.move_head(0, 1);
+        self.do_move(0, 1);
     }
     pub fn left(&mut self) {
         // print!("move_left\n");
-        self.move_head(-1, 0);
+        self.do_move(-1, 0);
     }
     pub fn right(&mut self) {
         // print!("move_right\n");
-        self.move_head(1, 0);
+        self.do_move(1, 0);
     }
-
-    pub fn get(&self, depth: u8) -> &Rope {
-        let mut rope = self;
+    pub fn get(&self, depth: u8) -> &RopeSegment {
+        let mut rope_segment = self;
         for _ in 0..depth {
-            match rope.next.as_deref() {
-                None => panic!("end of the rope!"),
-                Some(n) => rope = n,
+            match rope_segment.next.as_deref() {
+                None => panic!("I'm at the end of my rope_segment!"),
+                Some(n) => rope_segment = n,
             }
         }
-        rope
+        rope_segment
     }        
+    pub fn get_loc(&self, depth: u8) -> Point {
+        self.get(depth).loc.clone()
+    }
 }
+
+const ROPE_LEN: u8 = 10;
 
 pub fn solve() -> SolutionPair {
     let input = util::open_input("input/9.txt").lines();
 
     let mut visited_locations1: HashSet<Point> = HashSet::new();
-    let mut visited_locations2: HashSet<Point> = HashSet::new();
-
-    // let cbk = |point: Point| visited_locations1.insert(point);
-    let mut rope = Rope {
-        desc: '9',
-        head: Point { x: 0, y: 0 },
-        tail: Point { x: 0, y: 0 },
-        next: None,
-        // on_tail_move: cbk,
-    };
     visited_locations1.insert(Point { x: 0, y: 0 });
+
+    let mut visited_locations2: HashSet<Point> = HashSet::new();
     visited_locations2.insert(Point { x: 0, y: 0 });
 
-    for d in (0..8).rev() {
-        rope = Rope {
-            desc: char::from_digit(d, 10).unwrap(),
-            head: Point { x: 0, y: 0 },
-            tail: Point { x: 0, y: 0 },
-            next: Some(Box::from(rope)),
-            // on_tail_move: cbk,
+    let mut rope_segment = RopeSegment {
+        desc: char::from_digit(ROPE_LEN as u32, 36).unwrap(),
+        loc: Point { x: 0, y: 0 },
+        next: None,
+    };
+
+    for d in (0..ROPE_LEN).rev() {
+        rope_segment = RopeSegment {
+            desc: char::from_digit(d as u32, 36).unwrap(),
+            loc: Point { x: 0, y: 0 },
+            next: Some(Box::from(rope_segment)),
         };
     }
 
     for line in input {
         let line = line.unwrap();
         let cmd: Vec<&str> = line.trim().split(' ').collect();
-        let end_depth = 8;
         match cmd[..] {
             ["U", count] => {
                 for _ in 0..count.parse().unwrap() {
-                    rope.up();
-                    visited_locations1.insert(rope.get(0).tail.clone());
-                    visited_locations2.insert(rope.get(end_depth).tail.clone());
+                    rope_segment.up();
+                    visited_locations1.insert(rope_segment.get_loc(1));
+                    visited_locations2.insert(rope_segment.get_loc(ROPE_LEN - 1));
                 }
             }
             ["D", count] => {
                 for _ in 0..count.parse().unwrap() {
-                    rope.down();
-                    visited_locations1.insert(rope.get(0).tail.clone());
-                    visited_locations2.insert(rope.get(end_depth).tail.clone());
+                    rope_segment.down();
+                    visited_locations1.insert(rope_segment.get_loc(1));
+                    visited_locations2.insert(rope_segment.get_loc(ROPE_LEN - 1));
                 }
             }
             ["L", count] => {
                 for _ in 0..count.parse().unwrap() {
-                    rope.left();
-                    visited_locations1.insert(rope.get(0).tail.clone());
-                    visited_locations2.insert(rope.get(end_depth).tail.clone());
+                    rope_segment.left();
+                    visited_locations1.insert(rope_segment.get_loc(1));
+                    visited_locations2.insert(rope_segment.get_loc(ROPE_LEN - 1));
                 }
             }
             ["R", count] => {
                 for _ in 0..count.parse().unwrap() {
-                    rope.right();
-                    visited_locations1.insert(rope.get(0).tail.clone());
-                    visited_locations2.insert(rope.get(end_depth).tail.clone());
+                    rope_segment.right();
+                    visited_locations1.insert(rope_segment.get_loc(1));
+                    visited_locations2.insert(rope_segment.get_loc(ROPE_LEN - 1));
                 }
             }
             [..] => panic!("bad command: {:?}", cmd),
         }
         // print!("{:?}\n", cmd);
-        // print_state(&rope);
+        // print_state(&rope_segment);
 
+        // print_grid(&visited_locations1);
         // print_grid(&visited_locations2);
     }
 
